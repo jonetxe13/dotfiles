0c8d3568c4bac4f236b87e46ec8eb79299322050
jonetxe13 <jonetxe.sasia13@gmail.com>
2023-11-11 13:45:34 +0100

    primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+the nvim and kitty folder go into the .config folder directly
+
+the themes, oh my posh, p10 and powerlevel go inte the home directory
+
+
---
+++ script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#install nvim current latest
+nvim_latest_version=$(curl -s https://github.com/neovim/neovim/releases/latest | grep -Po '".*"' )
+# get version
+nvim_latest_version="${nvim_latest_version:47:-1}"
+# get download link
+nvim_base_download_link=https://github.com/neovim/neovim/releases/download/${nvim_latest_version}
+# append assets
+nvim_appimage_link=$nvim_base_download_link/nvim.appimage
+
+# download 
+ wget $nvim_appimage_link 
+ chmod +x nvim.appimage
+ cp nvim.appimage /bin/.
+
+#install kitty
+sudo apt-get install kitty gnome-tweaks zsh
+#install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#install powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+#the plugins and the powerlevel10k theme you have to mannually add them to your .zshrc file
+
+mv ./nvim ~/config/.
+mv ./kitty ~/config/.
+mv ./.themes ~/.
+mv ./.oh-my-zsh ~/.
+mv ./.p10k.zsh ~/.
+mv ./powerlevel10k ~/.
+
+echo "folders moved successfully!!"
